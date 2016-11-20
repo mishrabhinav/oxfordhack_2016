d696a7e16299da78aab42d2db9f64776f84f800e
Rakhilya Mekhtieva Lee <rm2815@ic.ac.uk>
2016-11-20 10:02:41 +0000

    Merge

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,127 @@ angular
     .controller('appCtrl', ['$scope', '$http', '$window', function appCtrl($scope, $http, $window) {
         var albumName;
         var imgData = [];
+        var newPoint;
+        var newGraphic;
+
+
+        require([
+            "esri/Map",
+            "esri/views/MapView",
+            "esri/geometry/Polyline",
+            "esri/geometry/Point",
+            "esri/geometry/Circle",
+            "esri/symbols/SimpleMarkerSymbol",
+            "esri/Graphic",
+            "esri/PopupTemplate",
+            "dojo/domReady!"
+        ], function(
+            Map,
+            MapView,
+            Polyline,
+            Circle,
+            Point,
+            SimpleMarkerSymbol,
+            Graphic,
+            PopupTemplate
+        ) {
+
+            var map = new Map({
+                basemap: "streets"
+            });
+
+            var view = new MapView({
+                center: [-1, 53],
+                container: "viewDiv",
+                map: map,
+                zoom: 6
+            });
+
+            // First create a line geometry (this is the Keystone pipeline)
+            var point = new Point({
+                latitude: 55.3,
+                longitude: 1.5
+            });
+
+            var markerSymbol = new SimpleMarkerSymbol({
+                color: [226, 119, 40],
+                outline: { // autocasts as new SimpleLineSymbol()
+                    color: [255, 255, 255],
+                    width: 2
+                }
+            });
+            /*
+             // Create a symbol for drawing the line
+             var lineSymbol = new SimpleLineSymbol({
+             color: [226, 119, 40],
+             width: 4
+             });
+             */
+            // Create an object for storing attributes related to the line
+            var pointAtt = {
+                Name: "Keystone Pipeline",
+                Owner: "TransCanada",
+                Length: "3,456 km"
+            };
+
+            /*******************************************
+             * Create a new graphic and add the geometry,
+             * symbol, and attributes to it. You may also
+             * add a simple PopupTemplate to the graphic.
+             * This allows users to view the graphic's
+             * attributes when it is clicked.
+             ******************************************/
+
+            function createPoint(x, y) {
+                return new Point({
+                    latitude: y,
+                    longitude: x
+                });
+            }
+
+            function setGraphic(point, attributes) {
+                return new Graphic({
+                    geometry: point,
+                    symbol: markerSymbol,
+                    attributes: attributes,
+                    popupTemplate: {
+                        title: attributes[0]
+                    }
+                })
+            }
+
+            var pointGraphic = new Graphic({
+                geometry: point,
+                symbol: markerSymbol,
+                attributes: pointAtt,
+
+                popupTemplate: {
+                    title: "{Name}",
+                    content: [{
+                        type: "fields",
+                        fieldInfos: [{
+                            fieldName: "Name"
+                        }, {
+                            fieldName: "Owner"
+                        }, {
+                            fieldName: "Length"
+                        }]
+                    }]
+                }
+            });
+
+            function addNewPoint(pointGraphic) {
+                // Add the line graphic to the view's GraphicsLayer
+                view.graphics.add(pointGraphic);
+            }
+
+        });
+
+
+
+
+
+
         $window.fbAsyncInit = function() {
             FB.init({
                 appId: '1151594398249523',
@@ -66,6 +187,10 @@ $scope.pjson = {};
                                       //$scope.emotions = response.data;
                                     });
                                 imgData = [];
+
+                                newPoint = createPoint(50, 1);
+                                newGraphic = setGraphic(newPoint, albumName)
+                                addNewPoint(newGraphic)
                             } else {
                                 console.log("nothing");
                             }
@@ -81,7 +206,7 @@ $scope.pjson = {};
                $scope.pjson = response.data;
                 console.log($scope.pjson);
 	    })
-	}
+	    }
 
         function getFacebookAlbum(str) {
             var n = str.split("&album_id=");
@@ -92,5 +217,7 @@ $scope.pjson = {};
             var n = str.split(" ");
             return n[n.length - 1];
         }
+
+
 
     }]);
