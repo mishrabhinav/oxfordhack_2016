42684021b45ecb0a11fae3b6bdb79c01ccb36466
Alessandro Bonardi <ab9515@ic.ac.uk>
2016-11-20 03:09:30 +0000

    Moved the facebook fetch to the push call. Some user interface changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,68 +2,84 @@
  * Created by Alessandro on 19/11/2016.
  */
 angular
-  .module('app', [])
-  .controller('appCtrl', ['$scope', '$http', '$window', function appCtrl($scope, $http, $window) {
-      var imgData;
-      $window.fbAsyncInit = function() {
-          FB.init({
-              appId: '1151594398249523',
-              status: true,
-              cookie: true,
-              xfbml: true,
-              version: 'v2.4'
-          });
+    .module('app', [])
+    .controller('appCtrl', ['$scope', '$http', '$window', function appCtrl($scope, $http, $window) {
+        var albumName;
+        var partialData = [];
+        var imgData = [];
+        $window.fbAsyncInit = function() {
+            FB.init({
+                appId: '1151594398249523',
+                status: true,
+                cookie: true,
+                xfbml: true,
+                version: 'v2.4'
+            });
 
-          FB.AppEvents.logPageView();
-          FB.getLoginStatus(function(response) {
-              if (response.status === 'connected') {
-                  console.log('Logged in.');
-                  FB.api(
-                          '/1283053661745309/photos?fields=link,name,images',
-                          function(response) {
-                              if (response && !response.error) {
-                                  imgData = response.data;
-                                  response.data.forEach(function(element){
-                                      console.log(element.images[0].source);
-                                  });
-                              } else {
-                                  console.log("nothing");
-                              }
-                          }
-                  );
-              }
-              else {
-                  console.log("not logged in");
-                  FB.login();
-              }
-          });
-      };
+            FB.AppEvents.logPageView();
 
-      (function(d, s, id){
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.id = id;
-          js.src = "https://connect.facebook.net/en_US/sdk.js";
-          fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+        };
 
-      var URL;
-      var pos;
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
 
-      function getURL(){
-          URL = document.getElementById("url").value;
-          pos = document.getElementById("pos").value;
-          console.log(URL);
-          console.log(pos);
+        var URL;
+        var pos;
 
-      }
+        function getURL(){
+            URL = document.getElementById("url").value;
+            pos = document.getElementById("pos").value;
+            console.log(URL);
+            console.log(pos);
+
+        }
 
         $scope.pushData = function() {
-          var postConfig = {headers : {'Content-Type': 'application/json'}}
+            console.log("In here");
+            URL = document.getElementById("url").value;
 
-          $http.post('/', imgData, postConfig)
-            .then(function(resp) {
-              console.log(resp)
-            })
+            FB.getLoginStatus(function(response) {
+                while (response.status !== 'connected') {
+                    console.log("not logged in");
+                    FB.login();
+                }
+                if (response.status === 'connected') {
+                    console.log('Logged in.');
+                    FB.api(
+                        //TODO Parse the actual URL, getting the numbers after album_id
+                        URL + 'photos?fields=link,name,images,event,album,place',
+                        function(response) {
+                            if (response && !response.error) {
+                                albumName = response.data[0].album.name;
+                                console.log(response);
+                                response.data.forEach(function(element){
+                                    partialData.push(element.images[0].source);
+                                });
+                                imgData = [albumName, partialData];
+                                console.log(imgData);
+
+                                var postConfig = {headers : {'Content-Type': 'application/json'}};
+                                console.log(imgData);
+                                $http.post('/', imgData, postConfig)
+                                    .then(function(resp) {
+                                        console.log(resp)
+                                    });
+                                partialData = [];
+                            } else {
+                                console.log("nothing");
+                            }
+                        }
+                    );
+                }
+
+            });
+
+            console.log("In here2");
+
         };
-      }])
+    }]);
